3e0fca30d06fcbb24091735c220d643fc798b8f2
aljangtze <aljangtze@163.com>
2018-08-30 09:51:23 +0800

    支持ie的问题

--- WarehouseInventory/layouts/header.php
+++ WarehouseInventory/layouts/header.php
@@ -3,8 +3,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+    <meta http-equiv="X-UA-Compatible" content=`"IE=edge"`>
+    <meta name="renderer" content="webkit">
     <title><?php if (!empty($page_title))
             echo remove_junk($page_title);
         elseif (!empty($user))
